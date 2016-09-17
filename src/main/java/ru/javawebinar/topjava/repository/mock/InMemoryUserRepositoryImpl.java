6d5267d024ceec56274bc44b82d65574eb03f4fd
iridiumhawk <hawk.tmn@gmail.com>
2016-09-18 01:41:41 +0500

    HW2 - UserRep

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,70 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * GKislin
+ * 15.06.2015.
+ */
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        LOG.info("delete " + id);
+        repository.remove(id);
+        return true;
+    }
+
+    @Override
+    public User save(User user) {
+        LOG.info("save " + user);
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        LOG.info("get " + id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        LOG.info("getAll");
+        List<User> userList = new ArrayList<>( repository.values());
+
+        Collections.sort(userList , (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return userList;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        LOG.info("getByEmail " + email);
+        return repository.values().stream().
+                filter(u -> u.getEmail().equals(email)).findFirst().get();
+
+    }
+
+
+
+
+
+}
+
