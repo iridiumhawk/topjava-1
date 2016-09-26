6f5b88dfaac0cb53ebf4c255ee1e66912029376c
iridiumhawk <hawk.tmn@gmail.com>
2016-09-27 02:39:46 +0500

    HomeWork3 - тесты подготовка

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,71 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.util.DbPopulator;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hawk on 27.09.2016.
+ */
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MealServiceTest {
+
+    @Autowired
+    protected MealService service;
+
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDates() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+}
