c17a0761f0de3e77398c705dc385209324c0f934
iridiumhawk <hawk.tmn@gmail.com>
2016-09-28 02:35:17 +0500

    HomeWork3 - тесты реализованы

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,6 +3,14 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+import static ru.javawebinar.topjava.util.TimeUtil.DATE_TIME_FORMATTER;
+
 /**
  * GKislin
  * 13.03.2015.
@@ -10,5 +18,22 @@ import ru.javawebinar.topjava.model.Meal;
 public class MealTestData {
 
     public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+
+    public static final List<Meal> USER_MEAL_LIST = Arrays.asList(
+            new Meal(1000,LocalDateTime.parse("2016-09-20 10:00", DATE_TIME_FORMATTER) , "Завтрак", 500),
+            new Meal(1001,LocalDateTime.parse("2016-09-20 19:00", DATE_TIME_FORMATTER) , "Ужин", 500),
+            new Meal(1002,LocalDateTime.parse("2016-09-20 14:00", DATE_TIME_FORMATTER) , "Обед", 1000)
+
+    );
+
+    public static final List<Meal> ADMIN_MEAL_LIST = Arrays.asList(
+            new Meal(1003,LocalDateTime.parse("2016-09-20 10:00", DATE_TIME_FORMATTER) , "Завтрак", 500),
+            new Meal(1004,LocalDateTime.parse("2016-09-20 19:00", DATE_TIME_FORMATTER) , "Ужин", 500),
+            new Meal(1005,LocalDateTime.parse("2016-09-20 14:00", DATE_TIME_FORMATTER) , "Обед", 1010)
+
+    );
 
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -6,9 +6,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.jdbc.JdbcMealRepositoryImpl;
+import ru.javawebinar.topjava.util.DbPopulatorMeal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static org.junit.Assert.*;
+import java.util.*;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+
+
 
 /**
  * Created by hawk on 27.09.2016.
@@ -20,13 +27,13 @@ import static org.junit.Assert.*;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MealServiceTest {
-
+    private static final Comparator<Meal> MEAL_COMPARATOR = Comparator.comparing(Meal::getDateTime).reversed();
     @Autowired
     protected MealService service;
 
 
     @Autowired
-    private DbPopulator dbPopulator;
+    private DbPopulatorMeal dbPopulator;
 
 
     @Before
@@ -36,27 +43,52 @@ public class MealServiceTest {
 
     @Test
     public void testGet() throws Exception {
+        Meal meal = service.get(USER_MEAL_LIST.get(0).getId(),USER_ID);
+        MATCHER.assertEquals(USER_MEAL_LIST.get(0), meal);
+
+        Meal mealAdmin = service.get(ADMIN_MEAL_LIST.get(0).getId(),ADMIN_ID);
+        MATCHER.assertEquals(ADMIN_MEAL_LIST.get(0), mealAdmin);
 
     }
+
 
     @Test
     public void testDelete() throws Exception {
+        service.delete(USER_MEAL_LIST.get(0).getId(),USER_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(USER_MEAL_LIST.get(1) , USER_MEAL_LIST.get(2) ), service.getAll(USER_ID));
+
+        service.delete(ADMIN_MEAL_LIST.get(0).getId(),ADMIN_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN_MEAL_LIST.get(1) , ADMIN_MEAL_LIST.get(2) ), service.getAll(ADMIN_ID));
+    }
+
+
+    @Test(expected = NotFoundException.class)
+    public void testNotFoundDelete() throws Exception {
+        service.delete(1,USER_ID);
+        service.delete(ADMIN_MEAL_LIST.get(0).getId(),USER_ID);
+        service.delete(USER_MEAL_LIST.get(0).getId(),ADMIN_ID);
 
     }
 
-    @Test
+//    @Test
     public void testGetBetweenDates() throws Exception {
 
     }
 
-    @Test
+//    @Test
     public void testGetBetweenDateTimes() throws Exception {
 
     }
 
     @Test
     public void testGetAll() throws Exception {
-
+      Collection<Meal> mealList = service.getAll(ADMIN_ID);
+        Collection<Meal> resultMeal = new ArrayList<>();
+//        resultMeal.addAll(USER_MEAL_LIST);
+        ADMIN_MEAL_LIST.sort(MEAL_COMPARATOR);
+        resultMeal.addAll(ADMIN_MEAL_LIST);
+//        resultMeal.stream().sorted (MEAL_COMPARATOR);
+        MATCHER.assertCollectionEquals(resultMeal, mealList);
     }
 
     @Test
