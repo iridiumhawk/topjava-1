dabfa55e681cf39693a3a3a039ac25d12f72e80b
iridiumhawk <hawk.tmn@gmail.com>
2016-10-03 02:04:21 +0500

    HW4 - Meal, JPA_Impl partly

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,7 +1,8 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -10,11 +11,28 @@ import java.time.LocalTime;
  * GKislin
  * 11.01.2015.
  */
+@NamedQueries({
+        @NamedQuery(name = "meal.del", query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = "meal.get", query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = "meal.getAll", query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.id DESC"),
+    /*    @NamedQuery(name = , query = "SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1"),
+        @NamedQuery(name = User.ALL_SORTED, query = "SELECT u FROM User u LEFT JOIN FETCH u.roles ORDER BY u.name, u.email"),*/
+})
+@Entity
+@Table(name = "meals")
 public class Meal extends BaseEntity {
+//    static final String DELETE = "";
+
+    @Column(name="date_time")
+    @NotEmpty
     private LocalDateTime dateTime;
 
+    @Column(name="description")
+    @NotEmpty
     private String description;
 
+    @Column(name="calories")
+    @NotEmpty
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
