a3642529dd61607dec518264457c405eed865954
iridiumhawk <hawk.tmn@gmail.com>
2016-10-16 00:44:51 +0500

    Master - 6.11 changed

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,26 +14,24 @@ import java.util.List;
  * gkislin
  * 02.10.2016
  */
-
 @Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
-    @Transactional
-    @Override
-    Meal save(Meal meal);
-
-    @Transactional
     @Modifying
-    @Query("DELETE FROM Meal m  WHERE m.user.id=:userId AND m.id=:id ")
+    @Transactional
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
     int delete(@Param("id") int id, @Param("userId") int userId);
 
+    @Override
+    Meal save(Meal item);
 
-    @Query("SELECT m FROM Meal m  WHERE m.user.id=:userId AND m.id=:id ")
-    Meal findOne(@Param("id") Integer id, @Param("userId") Integer userid);
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
+    List<Meal> getAll(@Param("userId") int userId);
 
-    @Query("SELECT m FROM Meal m  WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
-    List<Meal> findAll(@Param("userId") int userId);
-
-    @Query("SELECT m FROM Meal m  WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
+    @SuppressWarnings("JpaQlInspection")
+    @Query("SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
+
+    @Query("SELECT m FROM Meal m JOIN FETCH m.user WHERE m.id = ?1 and m.user.id = ?2")
+    Meal getWithUser(int id, int userId);
 }
