e2429abf3efdf464200a0d2f1137a3d7c8dec5f5
iridiumhawk <hawk.tmn@gmail.com>
2016-10-16 00:33:10 +0500

    Master - 6.11

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -2,8 +2,8 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -22,19 +22,14 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     @Autowired
     private CrudUserRepository crudUserRepository;
 
-
+    @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
         if (!meal.isNew() && get(meal.getId(), userId) == null) {
             return null;
         }
         meal.setUser(crudUserRepository.getOne(userId));
-        if (meal.isNew()) {
-            crudRepository.save(meal);
-            return meal;
-        } else {
-            return crudRepository.save(meal);
-        }
-
+        return crudRepository.save(meal);
     }
 
     @Override
@@ -44,16 +39,22 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal get(int id, int userId) {
-        return crudRepository.findOne(id, userId);
+        Meal meal = crudRepository.findOne(id);
+        return meal != null && meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.findAll(userId);
+        return crudRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return crudRepository.getBetween(startDate, endDate, userId);
+    }
+
+    @Override
+    public Meal getWithUser(int id, int userId) {
+        return crudRepository.getWithUser(id, userId);
     }
 }
