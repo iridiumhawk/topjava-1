69e4424af72f4f209bad9b0a46fee4bda657ad56
iridiumhawk <hawk.tmn@gmail.com>
2016-10-25 04:24:33 +0500

    HW7 - 2, mealrestcontroller added

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,14 +1,15 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealWithExceed;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -29,7 +30,8 @@ public class MealRestController extends AbstractMealController {
     }
 
     @Override
-    public void delete(int id) {
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") int id) {
         super.delete(id);
     }
 
@@ -40,17 +42,27 @@ public class MealRestController extends AbstractMealController {
     }
 
     @Override
-    public void update(Meal meal, int id) {
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Meal meal, @PathVariable("id") int id) {
         super.update(meal, id);
     }
 
-    @Override
-    public Meal create(Meal meal) {
-        return super.create(meal);
+
+    @PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public  ResponseEntity<Meal>  createMeal(@RequestBody Meal meal) {
+             Meal mealNew = super.create(meal);
+
+        try {
+            return ResponseEntity.created(new URI(REST_URL+"/"+mealNew.getId().toString())).body(mealNew);
+        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
     }
 
     @Override
-    public List<MealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+    @GetMapping(value = "/between", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getBetween(@RequestParam("datefrom") LocalDate startDate, LocalTime startTime, @RequestParam("dateto") LocalDate endDate, LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
