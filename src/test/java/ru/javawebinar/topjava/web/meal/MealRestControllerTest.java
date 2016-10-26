87fce8222dab3ded5d399e64038010ff483a691d
iridiumhawk <hawk.tmn@gmail.com>
2016-10-27 00:51:33 +0500

    HW7 -  test released

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -3,16 +3,17 @@ package ru.javawebinar.topjava.web.meal;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.time.Month;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static java.time.LocalDateTime.of;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -63,12 +64,31 @@ public class MealRestControllerTest extends AbstractControllerTest {
 
     @Test
     public void testUpdate() throws Exception {
+        Meal updated = new Meal(MEAL1);
+        updated.setDescription("UpdatedName");
+        mockMvc.perform(put(REST_URL + USER_MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk())
+                .andDo(print());
 
+        MATCHER.assertEquals(updated, mealService.get(MEAL1.getId(), USER_ID));
     }
 
     @Test
     public void testCreateMeal() throws Exception {
+        Meal mealExpect = new Meal(null, of(2015, Month.MAY, 29, 10, 0), "Завтрак", 100);
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(mealExpect)))
+                .andExpect(status().isCreated())
+                .andDo(print());
 
+        Meal returned = MATCHER.fromJsonAction(action);
+        mealExpect.setId(returned.getId());
+
+        MATCHER.assertEquals(mealExpect, returned);
+//        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, expected, USER), userService.getAll());
     }
 
     @Test
@@ -76,7 +96,9 @@ public class MealRestControllerTest extends AbstractControllerTest {
         mockMvc.perform(get(REST_URL + "/between?from=2015-05-30T09:00:00&to=2015-05-31T21:00:00"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(MATCHER.contentListMatcher(MEALS))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHEREXCEEDED.contentListMatcher(MealsUtil.getWithExceeded(MEALS, 2000)))
+//                .andExpect(MATCHER.contentListMatcher(MEALS))
         ;
 
 
